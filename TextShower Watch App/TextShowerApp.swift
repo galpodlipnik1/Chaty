4c4cfe97be4549ec698e9301cbbc26958ef20b01
galpodlipnik1 <itzgal2004@gmail.com>
2023-05-24 16:42:49 +0200

    Initial Commit

---
+++ TextShower Watch App/TextShowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextShowerApp.swift
+//  TextShower Watch App
+//
+//  Created by Gal Podlipnik on 24/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct TextShower_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
